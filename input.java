6997096c96d4b243c0d765277b5dacb82b742fa1
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:43:07 +0530

    Update input.java

--- input.java
+++ input.java
@@ -11,7 +11,8 @@ public class input {
         Scanner nu = new Scanner(System.in);  
         // Create a Scanner object
         System.out.println("Enter second name");
-        String user = nu.nextLine();  // Read user input
+        String user = nu.nextLine();  
+        // Read user input
         System.out.println("Username is: " + userName + " " + user);  // Output user input
     }
 }
