a682caba2e46ee46418507d5622ba390c44600ff
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:24:39 +0530

    Update condition.java

--- condition.java
+++ condition.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
 public class condition {
-  public static void main(String[] args) 
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  }
 }
