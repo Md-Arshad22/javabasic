008fc48adb08ace72046983384ef8f712eb4e283
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:14:18 +0530

    Update cond.java

--- cond.java
+++ cond.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
 public class cond {
-  public static void main(String[] args) 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+  }
 }
