c14232b0e024534f42e4bc639c7561120d64b80c
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:30:47 +0530

    Update input2.java

--- input2.java
+++ input2.java
@@ -1,1 +1,27 @@
+import java.util.Scanner ;
 
+public class inputt
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        System.out.println("Enter your city");
+        String city = sc.next();
+        System.out.println("Enter your trade");
+        String trade = sc.next();
+        System.out.println("Enter your phn");
+        int phn = sc.nextInt();
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("City : " + city);
+        System.out.println("trade : " + trade);
+        System.out.println("phn : " + phn);
+
+
+    }
+}
