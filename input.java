f0be9c86e577b8736f27a767e200930610fa2929
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:30:26 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;  // Import the Scanner class
 
+public class input {
+
+    public static void main(String[] args) {
+        Scanner no = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter first name");
+        String userName = no.nextLine();  // Read user input
+        Scanner nu = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter second name");
+        String user = nu.nextLine();  // Read user input
+        System.out.println("Username is: " + userName + " " + user);  // Output user input
+    }
+}
