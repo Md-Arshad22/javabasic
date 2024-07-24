e61729a09fc8167233082f18753991b65389bf6e
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:37:32 +0530

    Update sump.java

--- sump.java
+++ sump.java
@@ -2,7 +2,7 @@
 public class sump {
     public static void main(String[] args) {
         int a = 0;
-        for ( i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             a = a + i;
             System.out.println(a);
         }
