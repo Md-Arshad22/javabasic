9b29163a748e3f03e5750b7d8beb1d302bdac602
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:46:34 +0530

    Update input2.java

--- input2.java
+++ input2.java
@@ -7,6 +7,7 @@ public class input2
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = sc.nextLine();
+        
         System.out.println("Enter your age");
         int age = sc.nextInt();
         System.out.println("Enter your city");
