8c1dd81428349d3f60d260cca52985d2f019583f
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-24 10:27:03 +0530

    Update condition.java

--- condition.java
+++ condition.java
@@ -4,7 +4,13 @@ public class condition {
   Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
     if(a<=40) {
-    
+      System.out.println("fail");
+    }
+    else if(a<=80) {
+      System.out.println("pass");
+    }
+    else {
+      System.out.println("first class");
     }
   }
 }
